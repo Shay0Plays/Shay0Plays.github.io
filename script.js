c287c3538b7a1fdc4ec4e72896c083cb8b24b6d8
Shay0Plays <128226343+Shay0Plays@users.noreply.github.com>
2025-05-15 21:46:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ window.addEventListener('DOMContentLoaded', function() {
         "Anyone can love a rose, but it takes a great deal to love a leaf. It’s ordinary to love the beautiful, but it’s beautiful to love the ordinary.",
         "Wise men speak because they have something to say; Fools speak because they have to say something.",
         "It's not what happens to you, but how you react to it that matters.",
-        "Lust will cause a man to want something, even if he has everything"
+        "Lust will cause a man to want something, even if he has everything",
+        "Never argue with an idiot. He will drag you down to his level and beat you with experience.",
     ];
 
     // Pick a random quote
